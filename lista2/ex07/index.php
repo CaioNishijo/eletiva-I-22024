12e0559e90813b12f626249e83e43dcf6e8cf950
Caio Nishijo <110948758+CaioNishijo@users.noreply.github.com>
2024-08-25 12:26:16 -0300

    Update index.php

--- lista2/ex07/index.php
+++ lista2/ex07/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ex06</title>
+    <title>ex07</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
@@ -16,7 +16,7 @@
 		</div>
 		<div class="row m-5">
 			<div class="col">
-				<label class="form-label">Temperatura (°F)</label>
+				<label for="temp" class="form-label">Temperatura (°F)</label>
 				<input type="number" class="form-control" name="temp" id="temp" placeholder="Insira a temperatura em graus fahrenheit">
 			</div>
 		</div>
