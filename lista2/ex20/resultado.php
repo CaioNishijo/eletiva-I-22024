af85abf642fe24e97cd9a6423af01f83268956bb
Caio Nishijo <110948758+CaioNishijo@users.noreply.github.com>
2024-08-31 12:39:06 -0300

    Add files via upload

---
+++ lista2/ex20/resultado.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            try{    
+                (float)$distancia = $_POST["distancia"];
+                (float)$tempo = $_POST["tempo"];
+                (float)$velocidade = $distancia * $tempo;
+                echo"
+                <div class='row m-5'>
+                    <div class='col'>
+                        <h1 class='text-center'>A velocidade é $velocidade km/h</h1>
+                    </div>
+                </div>
+                ";
+
+            }catch(Exception $e){
+                echo"Erro: ".$e->getMessage();
+            }
+        }
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
